7f7ea2fc306601773ccbcd5e40ed3d0be2749c5c
Helena <helena.jagodnik@gmail.com>
2018-01-14 17:59:39 -0800

    Make menu hidden testing more comprehensive. Comment cleanup..

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,7 +26,7 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-        /* TODO: Write a test that loops through each feed
+        /* A test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -37,7 +37,7 @@ $(function() {
             });
          });
 
-        /* TODO: Write a test that loops through each feed
+        /* A test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -50,18 +50,16 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
     describe ('The menu', function(){
-        /* TODO: Write a test that ensures the menu element is
+        /* A test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
          it('Menu is hidden by default', function(){
-            var bodyClass = document.getElementsByTagName("body")[0].className;
-            expect(bodyClass).toContain("menu-hidden");
+            expect(document.body.classList).toContain("menu-hidden");
          });
-          /* TODO: Write a test that ensures the menu changes
+          /* A test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
@@ -70,48 +68,44 @@ $(function() {
         it('Menu changes visibility', function(){
             //menu display when clicked, doesn't have class menu-hidden
             $('.menu-icon-link').click();
-            var bodyClassBefore = document.querySelector("body").className;
-            expect(bodyClassBefore).not.toContain("menu-hidden");
+            expect(document.body.classList).not.toContain("menu-hidden");
 
             //hide menu when clicked again, has class menu-hidden
             $('.menu-icon-link').click();
-            var bodyClassAfter = document.querySelector("body").className;
-            expect(bodyClassAfter).toContain("menu-hidden");
+            expect(document.body.classList).toContain("menu-hidden");
         });
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function(){
-        /* TODO: Write a test that ensures when the loadFeed
+        /* A test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+
          //get first feed
          beforeEach(function(done){
-            loadFeed(0, function(){
-                done();
-            });
-         });
+            loadFeed(0, done);
+        });
 
          it('LoadFeed function loads feeds', function(){
             var entries = document.querySelectorAll(".feed .entry");
             expect(entries.length).not.toBe(0); 
         });
     });
-        /* TODO: Write a new test suite named "New Feed Selection" */
+
     describe('New Feed Selection', function(){
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /* A test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        
         //get old feed
         beforeEach(function(done){
-            loadFeed(0, function(){
-                done();
-            });
-         });
+            loadFeed(0, done);
+        });
+
         //content actually changes
         it('Content has changed', function(done){
             var oldEntries = document.querySelector(".feed").innerHTML;
